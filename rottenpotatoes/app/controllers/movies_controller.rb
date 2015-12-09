ba62e91a69c731286322f4a91c415e68111149d0
bsmit256 <bsmit256@uncc.edu>
2015-12-09 15:07:17 +0000

    got an 80

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:director, :title, :rating, :description, :release_date)
   end
 
   def show
@@ -60,5 +60,13 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
+  def same_director 
+    @movie = Movie.find params[:id]
+    if @movie.director and !@movie.director.empty?
+      @movies= Movie.where(:director=>@movie.director)
+     else
+       flash[:notice] = "'#{@movie.title}' has no director info."
+       redirect_to movies_path
+     end
+   end
 end
